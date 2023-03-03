5ac53a2c09e05e1adacf39d609cb17d95e4f1305
Lisa755 <115060723+Lisa755@users.noreply.github.com>
2023-03-03 09:15:45 +0200

    Add files via upload

---
+++ esa/dropdown.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title></title>
+</head>
+<body>
+
+<div class="navbar">
+    <div class="dropdown">
+        <button class="dropbtn">Home
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Dashboard</a>
+          
+
+          <a href="#">Customer Dashboard</a>
+          <a href="#">Supplier Dashboard</a>
+            <a href="#">item Dashboard</a>
+          <a href="#">Financial Dashboard</a>
+        </div>
+      </div>
+
+    <div class="dropdown">
+        <button class="dropbtn">Quick View
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Customers </a>
+          <a href="#">Supplers</a>
+          <a href="#">Items</a>
+            <a href="#">Bank Accounts</a>
+          <a href="#">Accounts</a>
+        </div>
+    </div>
+
+     <div class="dropdown">
+        <button class="dropbtn">Customers
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Lists</a>
+
+          <a href="#">Transactions</a>
+          <a href="#">Reports</a>
+            <a href="#">Bank Accounts</a>
+          <a href="#">Special</a>
+        </div>
+    </div>
+
+
+    <div class="dropdown">
+        <button class="dropbtn">Suppliers
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Add Supplier</a>
+
+          <a href="#">Lists</a>
+          <a href="#">Transactions</a>
+            <a href="#">Reports
+          <a href="#">Special</a>
+        </div>
+    </div>
+
+
+
+    <div class="dropdown">
+        <button class="dropbtn">Items
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Add Items</a>
+
+          <a href="#">Lists</a>
+          <a href="#">Transactions</a>
+            <a href="#">Reports
+          <a href="#">Special</a>
+        </div>
+    </div>
+
+
+    <div class="dropdown">
+        <button class="dropbtn">Banking
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Add Bank Or Credit Card</a>
+
+          <a href="#">Lists</a>
+          <a href="#">Transactions</a>
+            <a href="#">Reports
+          <a href="#">Special</a>
+        </div>
+    </div>
+
+
+
+    <div class="dropdown">
+        <button class="dropbtn">Accounts
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Add Account</a>
+
+          <a href="#">Lists</a>
+          <a href="#">Add Account Reporting Group</a>
+            <a href="#">Adjust Account Opening Balances</a>
+          <a href="#">Reports</a>
+        </div>
+    </div>
+
+
+     <div class="dropdown">
+        <button class="dropbtn">Reports
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Accounting inteligence Reports</a>
+
+          <a href="#">Customers</a>
+          <a href="#">Suppliers</a>
+            <a href="#">Items</a>
+          <a href="#">Sales And pruchases</a>
+          <a href="#">Banks And Creditcards</a>
+          <a href="#">VAT</a>
+            <a href="#">Financial Statements</a>
+            <a href="#">Asset Report</a>
+          <a href="#">Budget Report</a>
+            <a href="#">Other</a>
+            <a href="#">Accountant Reports</a>
+            
+
+
+        </div>
+    </div>
+    
+
+    <div class="dropdown">
+        <button class="dropbtn">Company
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Change Company Settings</a>
+
+          <a href="#">Company Notes And Settings</a>
+          <a href="#">Assets</a>
+            <a href="#">Budgets</a>
+          <a href="#">Analysis Codes</a>
+            <a href="#">Import Data</a>
+          <a href="#">Export Data</a>
+          <a href="#">Opening Balances</a>
+        </div>
+    </div>
+
+
+
+    <div class="dropdown">
+        <button class="dropbtn">Administration
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="#">Change Password</a>
+
+          <a href="#">My Profile</a>
+          <a href="#">Logout</a>
+            
+        </div>
+    </div>
+
+  <a href="sample.php">Sample</a>
+
+</div>
+
+
+<style type="text/css">
+ 
+</style>
+
+</body>
+</html>
